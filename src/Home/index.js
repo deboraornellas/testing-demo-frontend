ea027be20e7c1afcf77163d6b6833afa8a350909
Débora <debora.ornellas1@gmail.com>
2019-11-13 00:42:19 -0300

    Implement connexion to backend

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,37 +1,163 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, Fragment } from "react";
+import { withRouter } from "react-router-dom";
 import { Container, TitleStyle, MainTitleStyle } from "./style";
 import Button from "@material-ui/core/Button";
 import Input from "@material-ui/core/Input";
 import ITALogo from "./ITA_logo.png";
+import axios from "axios";
+
+const BACKEND_URL = "http://localhost:5000";
 
 const Home = props => {
-    return (
-        <Container>
-            <img src={ITALogo} style={{ width: "180px" }} />
-            <MainTitleStyle>
-                Seja bem vindo ao serviço de informação de alunos do ITA
-            </MainTitleStyle>
-            <p>
-                <strong>
-                    Aqui, você pode acessar dados importantes dos alunos: nome,
-                    curso e média acadêmica
-                </strong>
-            </p>
-            <p>
-                Digite o nome completo do aluno para visualizar suas
-                informações.
-            </p>
+  const [studentPageFormOpen, setStudentPageFormOpen] = useState(false);
+  const [newGradeFormOpen, setNewGradeFormOpen] = useState(false);
+  const [newStudentFormOpen, setNewStudentFormOpen] = useState(false);
+  const [addGradeMessage, setAddGradeMessage] = useState("");
+  const [addStudentMessage, setAddStudentMessage] = useState("");
 
-            <TitleStyle>Acessar perfil de um aluno:</TitleStyle>
-            <Input></Input>
-            <br />
-            <Button variant="contained" component={Link} to="/student-details">
-                Ver perfil
+  const openNewStudentForm = () => {
+    setNewStudentFormOpen(!newStudentFormOpen);
+    setAddStudentMessage("");
+  };
+
+  const openStudentPageForm = () => {
+    setStudentPageFormOpen(!studentPageFormOpen);
+  };
+
+  const openNewGradeForm = () => {
+    setNewGradeFormOpen(!newGradeFormOpen);
+    setAddGradeMessage("");
+  };
+
+  const redirectToStudentPage = event => {
+    const name = event.target[0].value;
+    event.preventDefault();
+    props.history.push({
+      pathname: "/student-details",
+      state: { name }
+    });
+  };
+
+  const sendNewStudentData = async event => {
+    const fullName = event.target[0].value;
+    const course = event.target[1].value;
+    const graduationYear = event.target[2].value;
+
+    event.preventDefault();
+    try {
+      await axios.post(`${BACKEND_URL}/new-student`, null, {
+        params: {
+          fullName,
+          course,
+          graduationYear
+        }
+      });
+      setAddStudentMessage("Aluno(a) adicionado(a) com sucesso");
+    } catch (e) {
+      console.log(e);
+      setAddStudentMessage("Não foi possível adicionar o(a) aluno(a)");
+    }
+  };
+
+  const sendNewGrade = async event => {
+    const fullName = event.target[0].value;
+    const newGrade = event.target[1].value;
+
+    event.preventDefault();
+    try {
+      await axios.post(`${BACKEND_URL}/add-grade`, null, {
+        params: {
+          fullName,
+          newGrade
+        }
+      });
+      setAddGradeMessage("Nota adicionada com sucesso.");
+    } catch (e) {
+      console.log(e);
+      setAddGradeMessage("Não foi possível adicionar a nota.");
+    }
+  };
+
+  return (
+    <Container>
+      <img src={ITALogo} style={{ width: "180px" }} alt="Logo do ITA" />
+      <MainTitleStyle>
+        Seja bem vindo ao serviço de informação de alunos do ITA
+      </MainTitleStyle>
+      <p>
+        <strong>
+          Aqui, você pode acessar dados importantes dos alunos: nome, curso e
+          média acadêmica.
+        </strong>
+      </p>
+      <p>
+        Digite o nome completo do(a) aluno(a) para visualizar suas informações.
+      </p>
+
+      <Button variant="contained" onClick={openStudentPageForm}>
+        Acessar perfil de um(a) aluno(a)
+      </Button>
+      <br />
+      {studentPageFormOpen && (
+        <Fragment>
+          <TitleStyle>Acessar perfil de um(a) aluno(a):</TitleStyle>
+          <form onSubmit={redirectToStudentPage}>
+            <Input type="text" placeholder="Nome do(a) aluno(a)" />
+            <br /> <br />
+            <Button variant="contained" type="submit">
+              Ver perfil
             </Button>
+          </form>
+          <br />
+        </Fragment>
+      )}
+      <Button variant="contained" onClick={openNewGradeForm}>
+        Enviar nova nota
+      </Button>
+      {newGradeFormOpen && (
+        <Fragment>
+          <br />
+          <form onSubmit={sendNewGrade}>
+            <Input type="text" placeholder="Nome do(a) aluno(a)" />
             <br />
-        </Container>
-    );
+            <Input type="text" placeholder="Nova nota (valor numérico)" />
+            <br />
+            <br />
+            <Button variant="contained" type="submit">
+              Enviar
+            </Button>
+          </form>
+          <p>
+            <strong>{addGradeMessage}</strong>
+          </p>
+        </Fragment>
+      )}
+      <br />
+      <Button variant="contained" onClick={openNewStudentForm}>
+        Adicionar novo(a) aluno(a) à plataforma
+      </Button>
+      <br />
+      {newStudentFormOpen && (
+        <Fragment>
+          <form onSubmit={sendNewStudentData}>
+            <Input type="text" placeholder="Nome do(a) aluno(a)" />
+            <br />
+            <Input type="text" placeholder="Curso do(a) aluno(a)" />
+            <br />
+            <Input type="text" placeholder="Ano de graduação" />
+            <br />
+            <br />
+            <Button variant="contained" type="submit">
+              Enviar
+            </Button>
+          </form>
+          <p>
+            <strong>{addStudentMessage}</strong>
+          </p>
+        </Fragment>
+      )}
+    </Container>
+  );
 };
 
-export default Home;
+export default withRouter(Home);
--- src/StudentDetails/index.js
+++ src/StudentDetails/index.js
@@ -1,26 +1,78 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Fragment, useEffect, useState } from "react";
+import { Link, withRouter } from "react-router-dom";
 import { Container, TitleStyle, MainTitleStyle } from "./style";
 import Button from "@material-ui/core/Button";
+import axios from "axios";
+
+const BACKEND_URL = "http://localhost:5000";
 
 const StudentDetails = props => {
-    return (
-        <Container>
-            <MainTitleStyle>Perfil Estudantil: {props.name} </MainTitleStyle>
-            <p>
-                <strong>Curso: {props.course}</strong>
-            </p>
-            <p>
-                Here you can add all the words you know from a new language and
-                test your memory.
-            </p>
+  const [studentInfo, setStudent] = useState();
+  const [studentNotFound, setStudentNotFound] = useState(false);
 
-            <TitleStyle>Média atual: {props.average}</TitleStyle>
-            <Button variant="contained" component={Link} to="/">
-                Voltar para a página inicial
-            </Button>
-        </Container>
-    );
+  useEffect(() => {
+    async function fetchData() {
+      const name = props.location.state.name;
+      try {
+        const response = await axios.get(`${BACKEND_URL}/student`, {
+          params: { fullName: name }
+        });
+        const fetchedStudentData = await response.data;
+        setStudent(fetchedStudentData);
+      } catch {
+        setStudentNotFound(true);
+        setTimeout(() => {
+          props.history.push({
+            pathname: "/"
+          });
+        }, 1500);
+      }
+    }
+    fetchData();
+  }, [props.history, props.location.state.name]);
+
+  return (
+    <Container>
+      {studentNotFound && (
+        <MainTitleStyle style={{ color: "red" }}>
+          Estudante não encontrado. Redirecionando...
+        </MainTitleStyle>
+      )}
+      {!studentNotFound && studentInfo && (
+        <Fragment>
+          <MainTitleStyle>
+            Perfil Estudantil: {studentInfo.fullName}{" "}
+          </MainTitleStyle>
+          <p>
+            <strong>Curso:</strong> {studentInfo.course}
+          </p>
+          <p>
+            <strong>Turma:</strong> {studentInfo.graduationYear}
+          </p>
+          <p>
+            <strong>Avatar:</strong>
+          </p>
+          <img
+            src={studentInfo.avatarSrc}
+            style={{ width: 200 }}
+            alt="Avatar do(a) estudante"
+          />
+
+          {studentInfo.average !== 0 && (
+            <TitleStyle>
+              Média atual: {studentInfo.average.toFixed(2)}
+            </TitleStyle>
+          )}
+          {studentInfo.average === 0 && (
+            <TitleStyle>Este aluno(a) não possui notas registradas.</TitleStyle>
+          )}
+          <Button variant="contained" component={Link} to="/">
+            Voltar para a página inicial
+          </Button>
+        </Fragment>
+      )}
+    </Container>
+  );
 };
 
-export default StudentDetails;
+export default withRouter(StudentDetails);
